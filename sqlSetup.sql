72654a3ad164a11f9ace42f28ab0ddaa53508b45
dartonus <callum.grant@verizon.net>
2016-03-25 16:44:44 -0400

    working on mysql usage
    created a new version of the webapp that draws from a php file. Need to
    set up a local web server on my end to verify expected functionality of
    the php. also included the sql code I used to set up my database.

---
+++ sqlSetup.sql
@@ -0,0 +1,36 @@
+/*
+Code to initialize small test database.
+*/
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sensors` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sensors`;
+
+DROP TABLE IF EXISTS sensordata;
+
+CREATE TABLE sensordata (
+	Date_Time varchar(50) NOT NULL,
+    Glennan INT NOT NULL,
+    Nord INT NOT NULL,
+    Olin_480 INT NOT NULL,
+    Olin_208 INT NOT NULL,
+    Rock_480 INT NOT NULL,
+    Sears_480 INT NOT NULL,
+    Sears_208 INT NOT NULL,
+    Tomlinson INT NOT NULL,
+    White INT NOT NULL,
+    Wick_208 INT NOT NULL,
+    Wick_480 INT NOT NULL,
+    Yost INT NOT NULL,
+    Olin_Sum INT NOT NULL,
+    Sears_Sum INT NOT NULL,
+    Wick_Sum INT NOT NULL
+);
+
+
+/* MySQL server by default runs with --secure-file-priv, only allowing user to use files in the Uploads directory. */
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/smallsample.csv'
+INTO TABLE sensordata
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
